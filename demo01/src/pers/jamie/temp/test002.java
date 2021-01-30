4de8d2856015249097973da50b4833546c6ad54d
InJamie <815138701@qq.com>
2021-01-30 11:33:57 +0800

    fix:dev提交部分冲突代码
    commit 21：12

---
+++ demo01/src/pers/jamie/temp/test002.java
@@ -0,0 +1,11 @@
+package pers.jamie.temp;
+
+public class test002 {
+    public static void main(String[] args) {
+
+        String temp = "102";
+        if ("101".equals(temp)||"102".equals(temp)||"104".equals(temp)){
+            System.out.println("同一证件。");
+        }
+    }
+}
