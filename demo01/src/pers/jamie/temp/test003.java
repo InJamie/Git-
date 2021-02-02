bde6bc5ae94b5c0d70f033254ee993247878345f
InJamie <815138701@qq.com>
2021-02-02 09:48:05 +0800

    fix:工作中的验证

---
+++ demo01/src/pers/jamie/temp/test003.java
@@ -0,0 +1,9 @@
+package pers.jamie.temp;
+
+public class test003 {
+    public static void main(String[] args) {
+        String a = "123456789.99";
+        String substring = a.substring(0, a.length() - 3);
+        System.out.println(substring);
+    }
+}
